6903cd04ded24a96c025e700e78ec94ac053bc2c
Leonardo Lüders <luderser@hotmail.com>
2026-09-05 23:03:59 -0300

    chore: activate typeorm synchorize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DeliveryModule } from './delivery/delivery.module';
-import { VoucherModule } from './voucher/voucher.module';
-import { CustomerModule } from './customer/customer.module';
-import { AddressModule } from './address/address.module';
-import { PayoutModule } from './payout/payout.module';
-import { TipModule } from './tip/tip.module';
-import { SettlementModule } from './settlement/settlement.module';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { PlaceModule } from './place/place.module';
-import { WorkTimeModule } from './work-time/work-time.module';
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import { UserModule } from "./user/user.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DeliveryModule } from "./delivery/delivery.module";
+import { VoucherModule } from "./voucher/voucher.module";
+import { CustomerModule } from "./customer/customer.module";
+import { AddressModule } from "./address/address.module";
+import { PayoutModule } from "./payout/payout.module";
+import { TipModule } from "./tip/tip.module";
+import { SettlementModule } from "./settlement/settlement.module";
+import { APP_FILTER, APP_GUARD } from "@nestjs/core";
+import { AllExceptionsFilter } from "./common/filters/all-exceptions.filter";
+import { JwtAuthGuard } from "./auth/guards/jwt-auth.guard";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { PlaceModule } from "./place/place.module";
+import { WorkTimeModule } from "./work-time/work-time.module";
 
 @Module({
   imports: [
@@ -41,13 +41,13 @@ import { WorkTimeModule } from './work-time/work-time.module';
       ],
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: "postgres",
       url: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false, // Neon usa certificado próprio
       },
       autoLoadEntities: true, // se já usa isso, mantém
-      synchronize: false, // NUNCA true em produção
+      synchronize: true, // NUNCA true em produção
     }),
     PlaceModule,
     WorkTimeModule,
@@ -67,4 +67,4 @@ import { WorkTimeModule } from './work-time/work-time.module';
     },
   ],
 })
-export class AppModule { }
+export class AppModule {}
