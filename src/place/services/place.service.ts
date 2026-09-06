a8799392bf765165541ee4f0997f6165509f4bc6
Leonardo Lüders <luderser@hotmail.com>
2026-09-05 23:26:05 -0300

    fix: work time always have isDefault field as true when creating with a place.

--- src/place/services/place.service.ts
+++ src/place/services/place.service.ts
@@ -3,22 +3,22 @@ import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
-} from '@nestjs/common';
-import { Place } from '../entities/place.entity';
+} from "@nestjs/common";
+import { Place } from "../entities/place.entity";
 import {
   EntityManager,
   FindOptionsOrder,
   FindOptionsOrderValue,
   FindOptionsWhere,
   Repository,
-} from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { CreatePlaceDto } from '../dto/create-place.dto';
-import { AddressService } from 'src/address/address.service';
-import { User } from 'src/user/entities/user.entity';
-import { UpdatePlaceDto } from '../dto/update-place.dto';
-import { WorkTimeService } from 'src/work-time/services/work-time.service';
-import { DataSource } from 'typeorm';
+} from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreatePlaceDto } from "../dto/create-place.dto";
+import { AddressService } from "src/address/address.service";
+import { User } from "src/user/entities/user.entity";
+import { UpdatePlaceDto } from "../dto/update-place.dto";
+import { WorkTimeService } from "src/work-time/services/work-time.service";
+import { DataSource } from "typeorm";
 
 @Injectable()
 export class PlaceService {
@@ -42,7 +42,7 @@ export class PlaceService {
   }
 
   async create(dto: CreatePlaceDto, owner: User) {
-    return this.dataSource.transaction(async manager => {
+    return this.dataSource.transaction(async (manager) => {
       const address = await this.addressService.create(
         dto.address,
         true,
@@ -52,7 +52,7 @@ export class PlaceService {
         ? await this.addressService.create(dto.postalBox, true, manager)
         : address;
       const workTime = await this.workTimeService.create(
-        dto.workTime,
+        { ...dto.workTime, isDefault: true },
         true,
         manager,
       );
@@ -70,7 +70,7 @@ export class PlaceService {
   }
 
   async update(place: Place, dto: UpdatePlaceDto) {
-    return this.dataSource.transaction(async manager => {
+    return this.dataSource.transaction(async (manager) => {
       place.name = dto.name ?? place.name;
       place.businessName = dto.businessName ?? place.businessName;
       place.phone = dto.phone ?? place.phone;
@@ -85,7 +85,7 @@ export class PlaceService {
   }
 
   async updateCode(place: Place, code: string) {
-    await this.failIfExists('code', code);
+    await this.failIfExists("code", code);
     const updated = await this.placeRepository.save({ ...place, code });
     return this.findOneByOrFail({ id: updated.id });
   }
@@ -97,7 +97,7 @@ export class PlaceService {
     const place = await this.findOneBy(placeData, manager);
 
     if (!place) {
-      throw new NotFoundException('Esse estabelecimento não existe.');
+      throw new NotFoundException("Esse estabelecimento não existe.");
     }
 
     return place;
@@ -138,18 +138,18 @@ export class PlaceService {
 
     if (place.code === process.env.DEFAULT_PLACE_CODE) {
       throw new UnauthorizedException(
-        'Não é possível remover o estabelecimento padrão',
+        "Não é possível remover o estabelecimento padrão",
       );
     }
 
-    const isOwner = place.owners.some(item => item.id === user.id);
+    const isOwner = place.owners.some((item) => item.id === user.id);
 
     if (!isOwner) {
-      throw new UnauthorizedException('Acesso negado');
+      throw new UnauthorizedException("Acesso negado");
     }
 
     const { workTimes } = place;
-    const workTimesId = workTimes.map(workTime => workTime.id);
+    const workTimesId = workTimes.map((workTime) => workTime.id);
 
     // Conversa com Claude sobre N+1 queries
     for (const id of workTimesId) {
